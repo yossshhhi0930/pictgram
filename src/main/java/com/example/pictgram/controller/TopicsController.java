44b8e192c1c29491ccb8dc34a2924ab3fc971e64
yoshmi <yossshhhi@gmail.com>
2023-10-16 16:30:49 +0900

    メール送信を追加する

--- src/main/java/com/example/pictgram/controller/TopicsController.java
+++ src/main/java/com/example/pictgram/controller/TopicsController.java
@@ -44,6 +44,8 @@ import com.example.pictgram.entity.Favorite;
 import com.example.pictgram.form.FavoriteForm;
 import com.example.pictgram.entity.Comment;
 import com.example.pictgram.form.CommentForm;
+import org.thymeleaf.context.Context;
+import com.example.pictgram.service.SendMailService;
 
 @Controller
 public class TopicsController {
@@ -64,6 +66,9 @@ public class TopicsController {
 
     @Value("${image.local:false}")
     private String imageLocal;
+    
+    @Autowired
+    private SendMailService sendMailService;
 
     @GetMapping(path = "/topics")
     public String index(Principal principal, Model model) throws IOException {
@@ -192,6 +197,12 @@ public class TopicsController {
         redirAttrs.addFlashAttribute("class", "alert-info");
         redirAttrs.addFlashAttribute("message", messageSource.getMessage("topics.create.flash.2", new String[] {}, locale));
 
+        Context context = new Context();
+        context.setVariable("title", "【Pictgram】新規投稿");
+        context.setVariable("name", user.getUsername());
+        context.setVariable("description", entity.getDescription());
+        sendMailService.sendMail(context);
+        
         return "redirect:/topics";
     }
 
